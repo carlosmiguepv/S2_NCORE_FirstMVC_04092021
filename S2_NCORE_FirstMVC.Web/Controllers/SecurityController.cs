2c4c26b2c50ed68496f0a95757f1172e026d1a7d
carlos <carlos@LAPTOP-9Q6APE6P>
2021-10-15 04:23:00 -0500

    Add project files.

---
+++ S2_NCORE_FirstMVC.Web/Controllers/SecurityController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace S2_NCORE_FirstMVC.Web.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
