d9cdd9126f8227fc774fe9253802235c5cbc04b9
carlos <carlos@LAPTOP-9Q6APE6P>
2021-10-15 08:32:14 -0500

    ViewModel in Marketing

--- S2_NCORE_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
+++ S2_NCORE_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using S2_NCORE_FirstMVC.Web.Areas.Marketing.Models;
 
 namespace S2_NCORE_FirstMVC.Web.Areas.Marketing.Controllers
 {
@@ -12,6 +15,23 @@ namespace S2_NCORE_FirstMVC.Web.Areas.Marketing.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        public IActionResult ProductsViewModel()
+        {
+            var products = GetProductsJsonLocal();
+            return View(products);
+        }
+
+
+        public IEnumerable<Product> GetProductsJsonLocal()
+        {
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Area\\Marketing\\Data\\products.json");
+            var json = System.IO.File.ReadAllText(folder);
+
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+            return products;
         }
     }
 }
---
+++ S2_NCORE_FirstMVC.Web/Areas/Marketing/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S2_NCORE_FirstMVC.Web.Areas.Marketing.Models
+{
+    public class Product
+    {
+
+        public string Id { get; set; }
+        public string Maker{ get; set; }
+
+        public string Img { get; set; }
+
+        public string Url{ get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
